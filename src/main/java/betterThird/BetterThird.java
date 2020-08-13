cbfb05f0c03c888b019bd70f1e84c7f6deff7d03
nferris165 <51347535+nferris165@users.noreply.github.com>
2020-08-13 14:46:44 -0400

    Initial commit

---
+++ src/main/java/betterThird/BetterThird.java
@@ -0,0 +1,221 @@
+package betterThird;
+
+import basemod.BaseMod;
+import basemod.ModLabeledToggleButton;
+import basemod.ModPanel;
+import basemod.ReflectionHacks;
+import basemod.eventUtil.AddEventParams;
+import basemod.eventUtil.EventUtils;
+import basemod.interfaces.*;
+import betterThird.util.TextureLoader;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.evacipated.cardcrawl.mod.stslib.Keyword;
+import com.evacipated.cardcrawl.modthespire.lib.SpireConfig;
+import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
+import com.google.gson.Gson;
+import com.megacrit.cardcrawl.audio.Sfx;
+import com.megacrit.cardcrawl.audio.SoundMaster;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.helpers.FontHelper;
+import com.megacrit.cardcrawl.localization.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Properties;
+
+@SuppressWarnings("unused")
+
+@SpireInitializer
+public class BetterThird implements
+        EditCardsSubscriber,
+        EditRelicsSubscriber,
+        EditStringsSubscriber,
+        EditKeywordsSubscriber,
+        EditCharactersSubscriber,
+        PostDeathSubscriber,
+        PostInitializeSubscriber{
+
+    public static final Logger logger = LogManager.getLogger(BetterThird.class.getName());
+
+    //mod settings
+    public static Properties defaultSettings = new Properties();
+    public static final String option_limit_settings = "ascensionLimit";
+    public static boolean optionLimit = false;
+
+    private static final String MODNAME = "Better Third";
+    private static final String AUTHOR = "Nichilas";
+    private static final String DESCRIPTION = "A mod to add interesting or dynamic third options to many binary events.";
+
+    private static final String BADGE_IMAGE = "betterThirdResources/images/Badge.png";
+
+    private static final String AUDIO_PATH = "betterThirdResources/audio/";
+
+    private static final String modID = "betterThird";
+
+
+    //Image Directories
+    public static String makeCardPath(String resourcePath) {
+        return modID + "Resources/images/cards/" + resourcePath;
+    }
+
+    public static String makeEventPath(String resourcePath) {
+        return modID + "Resources/images/events/" + resourcePath;
+    }
+
+    public static String makeMonsterPath(String resourcePath) {
+        return modID + "Resources/images/monsters/" + resourcePath;
+    }
+
+    public static String makeOrbPath(String resourcePath) {
+        return modID + "Resources/images/orbs/" + resourcePath;
+    }
+
+    public static String makePowerPath(String resourcePath) {
+        return modID + "Resources/images/powers/" + resourcePath;
+    }
+
+    public static String makeRelicPath(String resourcePath) {
+        return modID + "Resources/images/relics/" + resourcePath;
+    }
+
+    public static String makeRelicOutlinePath(String resourcePath) {
+        return modID + "Resources/images/relics/outline/" + resourcePath;
+    }
+
+    public static String makeUIPath(String resourcePath) {
+        return modID + "Resources/images/ui/" + resourcePath;
+    }
+
+    public static String makeVfxPath(String resourcePath) {
+        return modID + "Resources/images/vfx/" + resourcePath;
+    }
+
+
+    public BetterThird() {
+        BaseMod.subscribe(this);
+
+        logger.info("Adding mod settings");
+        defaultSettings.setProperty(option_limit_settings, "FALSE");
+        try {
+            SpireConfig config = new SpireConfig("betterThird", "betterThirdConfig", defaultSettings);
+            config.load();
+            optionLimit = config.getBool(option_limit_settings);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void initialize() {
+        BetterThird betterThird = new BetterThird();
+    }
+
+    public void receiveEditPotions() {
+        //BaseMod.addPotion(NewPotion.class, SLUMBERING_POTION_RUST, SLUMBERING_TEAL, SLUMBERING_POTION_RUST, NewPotion.POTION_ID, TheSlumbering.Enums.THE_SLUMBERING);
+    }
+
+    @Override
+    public void receiveEditCards() {
+
+    }
+
+    @Override
+    public void receiveEditCharacters() {
+        receiveEditPotions();
+    }
+
+    @Override
+    public void receiveEditKeywords() {
+        Gson gson = new Gson();
+        String json = Gdx.files.internal(modID + "Resources/localization/eng/Keyword-Strings.json").readString(String.valueOf(StandardCharsets.UTF_8));
+        com.evacipated.cardcrawl.mod.stslib.Keyword[] keywords = gson.fromJson(json, com.evacipated.cardcrawl.mod.stslib.Keyword[].class);
+
+        if (keywords != null) {
+            for (Keyword keyword : keywords) {
+                BaseMod.addKeyword(modID.toLowerCase(), keyword.PROPER_NAME, keyword.NAMES, keyword.DESCRIPTION);
+            }
+        }
+    }
+
+    @Override
+    public void receiveEditRelics() {
+
+    }
+
+    @Override
+    public void receiveEditStrings() {
+        BaseMod.loadCustomStringsFile(CardStrings.class,
+                modID + "Resources/localization/eng/Card-Strings.json");
+        BaseMod.loadCustomStringsFile(CharacterStrings.class,
+                modID + "Resources/localization/eng/Character-Strings.json");
+        BaseMod.loadCustomStringsFile(EventStrings.class,
+                modID + "Resources/localization/eng/Event-Strings.json");
+        BaseMod.loadCustomStringsFile(MonsterStrings.class,
+                modID + "Resources/localization/eng/Monster-Strings.json");
+        BaseMod.loadCustomStringsFile(OrbStrings.class,
+                modID + "Resources/localization/eng/Orb-Strings.json");
+        BaseMod.loadCustomStringsFile(PotionStrings.class,
+                modID + "Resources/localization/eng/Potion-Strings.json");
+        BaseMod.loadCustomStringsFile(PowerStrings.class,
+                modID + "Resources/localization/eng/Power-Strings.json");
+        BaseMod.loadCustomStringsFile(RelicStrings.class,
+                modID + "Resources/localization/eng/Relic-Strings.json");
+        BaseMod.loadCustomStringsFile(UIStrings.class,
+                modID + "Resources/localization/eng/UI-Strings.json");
+    }
+
+    private void loadAudio() {
+        HashMap<String, Sfx> map = (HashMap<String, Sfx>) ReflectionHacks.getPrivate(CardCrawlGame.sound, SoundMaster.class, "map");
+        //map.put("Pop", new Sfx(AUDIO_PATH + "pop.ogg", false));
+    }
+
+    public static String makeID(String idText) {
+        return modID + ":" + idText;
+    }
+
+    @Override
+    public void receivePostInitialize() {
+        Texture badgeTexture = TextureLoader.getTexture(BADGE_IMAGE);
+        ModPanel settingsPanel = new ModPanel();
+
+        ModLabeledToggleButton ascLimitButton = new ModLabeledToggleButton("Disables Dispel option.",
+                350.0f, 750.0f, Settings.CREAM_COLOR, FontHelper.charDescFont,
+                optionLimit,
+                settingsPanel,
+                (label) -> {},
+                (button) -> {
+
+                    optionLimit = button.enabled;
+                    try {
+                        SpireConfig config = new SpireConfig("betterThird", "betterThirdConfig", defaultSettings);
+                        config.setBool(option_limit_settings, optionLimit);
+                        config.save();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                });
+
+
+        settingsPanel.addUIElement(ascLimitButton);
+        BaseMod.registerModBadge(badgeTexture, MODNAME, AUTHOR, DESCRIPTION, settingsPanel);
+
+        //events
+        BaseMod.addEvent(new AddEventParams.Builder(BetterThirdEvent.ID, BetterThirdEvent.class)
+                .eventType(EventUtils.EventType.SHRINE).create());
+
+        //audio
+        loadAudio();
+    }
+
+    @Override
+    public void receivePostDeath() {
+        customMetrics metrics = new customMetrics();
+
+        Thread t = new Thread(metrics);
+        t.setName("Metrics");
+        t.start();
+    }
+}
---
+++ src/main/java/betterThird/util/customMetrics.java
@@ -0,0 +1,197 @@
+package betterThird.util;
+
+import betterThird.BetterThird;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Net;
+import com.badlogic.gdx.net.HttpRequestBuilder;
+import com.evacipated.cardcrawl.modthespire.Loader;
+import com.google.gson.Gson;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.rooms.VictoryRoom;
+import com.megacrit.cardcrawl.screens.DeathScreen;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.UUID;
+
+public class customMetrics implements Runnable {
+
+    private HashMap<Object, Object> params = new HashMap<>();
+    private Gson gson = new Gson();
+    private long lastPlaytimeEnd;
+    private boolean foundEvent = false;
+    public static final SimpleDateFormat timestampFormatter = new SimpleDateFormat("yyyyMMddHHmmss");
+
+    public static final String URL = "https://metricsanalysis.azurewebsites.net/metrics";
+
+
+    private void addData(Object key, Object value)
+    {
+        this.params.put(key, value);
+    }
+
+    private void sendPost()
+    {
+        LinkedHashMap<String, Serializable> event = new LinkedHashMap<>();
+        // TODO: UPDATES
+        event.put("title", "BetterThird");
+        event.put("event", this.params);
+        event.put("name", CardCrawlGame.playerName);
+        event.put("alias", CardCrawlGame.alias);
+
+        event.put("time", Long.valueOf(System.currentTimeMillis() / 1000L));
+        String data = this.gson.toJson(event);
+        HttpRequestBuilder requestBuilder = new HttpRequestBuilder();
+
+        Net.HttpRequest httpRequest = requestBuilder.newRequest().method("POST").url(URL).header("Content-Type", "application/json").header("Accept", "application/json").header("User-Agent", "curl/7.43.0").build();
+        httpRequest.setContent(data);
+        Gdx.net.sendHttpRequest(httpRequest, new Net.HttpResponseListener()
+        {
+            public void handleHttpResponse(Net.HttpResponse httpResponse) {
+                BetterThird.logger.info("Metrics: http request response: " + httpResponse.getResultAsString());
+            }
+
+            public void failed(Throwable t) {
+                BetterThird.logger.info("Metrics: http request failed: " + t.toString());
+            }
+
+            public void cancelled() {
+                BetterThird.logger.info("Metrics: http request cancelled.");
+            }
+        });
+    }
+
+    private void gatherAllData()
+    {
+        //Boolean death = AbstractDungeon.deathScreen.isVictory;
+        Boolean victory = AbstractDungeon.getCurrRoom() instanceof VictoryRoom;
+        addData("play_id", UUID.randomUUID().toString());
+        addData("build_version", CardCrawlGame.TRUE_VERSION_NUM);
+        addData("seed_played", Settings.seed.toString());
+        //addData("chose_seed", Boolean.valueOf(Settings.seedSet));
+        //addData("seed_source_timestamp", Long.valueOf(Settings.seedSourceTimestamp));
+        //addData("is_daily", Boolean.valueOf(Settings.isDailyRun));
+        //addData("special_seed", Settings.specialSeed);
+        //addData("is_trial", Boolean.valueOf(Settings.isTrial));
+        //addData("is_endless", Boolean.valueOf(Settings.isEndless));
+        /*
+        if (death)
+        {
+            AbstractPlayer player = AbstractDungeon.player;
+            CardCrawlGame.metricData.current_hp_per_floor.add(Integer.valueOf(player.currentHealth));
+            CardCrawlGame.metricData.max_hp_per_floor.add(Integer.valueOf(player.maxHealth));
+            CardCrawlGame.metricData.gold_per_floor.add(Integer.valueOf(player.gold));
+        }
+        */
+        //addData("is_ascension_mode", Boolean.valueOf(AbstractDungeon.isAscensionMode));
+        addData("ascension_level", Integer.valueOf(AbstractDungeon.ascensionLevel));
+
+        //addData("neow_bonus", CardCrawlGame.metricData.neowBonus);
+        //addData("neow_cost", CardCrawlGame.metricData.neowCost);
+
+        //addData("is_beta", Boolean.valueOf(Settings.isBeta));
+        //addData("is_prod", Boolean.valueOf(Settings.isDemo));
+        addData("victory", victory);
+        addData("floor_reached", Integer.valueOf(AbstractDungeon.floorNum));
+        addData("score", Integer.valueOf(DeathScreen.calcScore(victory)));
+        this.lastPlaytimeEnd = (System.currentTimeMillis() / 1000L);
+        addData("timestamp", Long.valueOf(this.lastPlaytimeEnd));
+        //addData("local_time", timestampFormatter.format(Calendar.getInstance().getTime()));
+        addData("playtime", Float.valueOf(CardCrawlGame.playtime));
+        addData("player_experience", Long.valueOf(Settings.totalPlayTime));
+        addData("master_deck", getDeck());
+        //addData("relics", AbstractDungeon.player.getRelicNames());
+        //addData("gold", Integer.valueOf(AbstractDungeon.player.gold));
+        //addData("campfire_rested", Integer.valueOf(CardCrawlGame.metricData.campfire_rested));
+        //addData("campfire_upgraded", Integer.valueOf(CardCrawlGame.metricData.campfire_upgraded));
+        //addData("purchased_purges", Integer.valueOf(CardCrawlGame.metricData.purchased_purges));
+        //addData("potions_floor_spawned", CardCrawlGame.metricData.potions_floor_spawned);
+        //addData("potions_floor_usage", CardCrawlGame.metricData.potions_floor_usage);
+        //addData("current_hp_per_floor", CardCrawlGame.metricData.current_hp_per_floor);
+        //addData("max_hp_per_floor", CardCrawlGame.metricData.max_hp_per_floor);
+        //addData("gold_per_floor", CardCrawlGame.metricData.gold_per_floor);
+        //addData("path_per_floor", CardCrawlGame.metricData.path_per_floor);
+        //addData("path_taken", CardCrawlGame.metricData.path_taken);
+        //addData("items_purchased", CardCrawlGame.metricData.items_purchased);
+        //addData("item_purchase_floors", CardCrawlGame.metricData.item_purchase_floors);
+        //addData("items_purged", CardCrawlGame.metricData.items_purged);
+        //addData("items_purged_floors", CardCrawlGame.metricData.items_purged_floors);
+        //addData("card_choices", CardCrawlGame.metricData.card_choices);
+        //addData("boss_relics", CardCrawlGame.metricData.boss_relics);
+        //addData("damage_taken", CardCrawlGame.metricData.damage_taken);
+        //addData("potions_obtained", CardCrawlGame.metricData.potions_obtained);
+        //addData("relics_obtained", CardCrawlGame.metricData.relics_obtained);
+        //addData("campfire_choices", CardCrawlGame.metricData.campfire_choices);
+
+        addData("character_chosen", AbstractDungeon.player.chosenClass.name());
+
+
+        //addData("event_choices", CardCrawlGame.metricData.event_choices);
+        addData("heal_limit", BetterThird.optionLimit);
+        addData("mods", getModList());
+    }
+
+    private String getModList(){
+        StringBuilder retVal = new StringBuilder();
+        String mod;
+        for(int i = 0; i < Loader.MODINFOS.length; ++i) {
+            if(i != 0){
+                retVal.append("-|-");
+            }
+            if(Loader.MODINFOS[i].Name != null){
+                mod = StringUtils.substring(Loader.MODINFOS[i].Name, 0, 30);
+                mod = mod.replace("'", "");
+                mod = mod.replace("`", "");
+            }
+            else{
+                mod = " ";
+            }
+            retVal.append(mod);
+            if(retVal.length() >= 1950){
+                retVal.append("---MAX SIZE REACHED!!!");
+                break;
+            }
+        }
+        return retVal.toString();
+    }
+
+    private HashMap getDeck(){
+        HashMap<String, Integer> map = new HashMap<>();
+        for(AbstractCard card: AbstractDungeon.player.masterDeck.group){
+            if(!map.containsKey(card.name)){
+                map.put(card.name, 1);
+            }
+            else{
+                int x = map.get(card.name);
+                x++;
+                map.remove(card.name);
+                map.put(card.name, x);
+            }
+        }
+        return map;
+    }
+
+    public void run()
+    {
+        for(HashMap map : CardCrawlGame.metricData.event_choices){
+            if(map.get("event_name").equals(BetterThirdEvent.ID)){
+                foundEvent = true;
+                addData("event_choice", map);
+                break;
+            }
+        }
+
+        if (foundEvent) {
+            if (Settings.UPLOAD_DATA && Settings.isStandardRun() && !Settings.seedSet && !Settings.isEndless) {
+                gatherAllData();
+                sendPost();
+            }
+        }
+    }
+}
