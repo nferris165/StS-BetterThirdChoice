f9807440da47b528c44f0e183fda41530289d007
nferris165 <51347535+nferris165@users.noreply.github.com>
2020-08-15 17:01:50 -0400

    shining light event
    added basics of event
    added basic text

--- src/main/java/betterThird/events/BetterShiningEvent.java
+++ src/main/java/betterThird/events/BetterShiningEvent.java
@@ -5,13 +5,15 @@ import com.badlogic.gdx.math.MathUtils;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.cards.colorless.Apotheosis;
+import com.megacrit.cardcrawl.cards.status.Burn;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.events.AbstractEvent;
 import com.megacrit.cardcrawl.events.AbstractImageEvent;
 import com.megacrit.cardcrawl.localization.EventStrings;
 import com.megacrit.cardcrawl.vfx.UpgradeShineEffect;
+import com.megacrit.cardcrawl.vfx.cardManip.ShowCardAndObtainEffect;
 import com.megacrit.cardcrawl.vfx.cardManip.ShowCardBrieflyEffect;
 import com.megacrit.cardcrawl.vfx.combat.FlashAtkImgEffect;
 
@@ -26,9 +28,9 @@ public class BetterShiningEvent extends AbstractImageEvent {
     private static final String[] OPTIONS = eventStrings.OPTIONS;
     private static final String IMG = "images/events/shiningLight.jpg";
 
-    private static final String AGREE_DIALOG;
-    private static final String DISAGREE_DIALOG;
-    private int damage = 0;
+    private static final String AGREE_DIALOG, EMBRACE_DIALOG, DISAGREE_DIALOG;
+    private int damage, burnChance;
+    private AbstractCard card, burn;
     private static final float HP_LOSS_PERCENT = 0.2F;
     private static final float A_2_HP_LOSS_PERCENT = 0.3F;
     private CUR_SCREEN screen;
@@ -38,19 +40,24 @@ public class BetterShiningEvent extends AbstractImageEvent {
         super(NAME, DESCRIPTIONS[0], IMG);
 
         this.optionsChosen = "";
-        this.screen = CUR_SCREEN.INTRO;// 37
-        if (AbstractDungeon.ascensionLevel >= 15) {// 46
-            this.damage = MathUtils.round((float)AbstractDungeon.player.maxHealth * A_2_HP_LOSS_PERCENT);// 47
+        this.card = new Apotheosis();
+        this.burn = new Burn();
+        this.burnChance = 75;
+        this.screen = CUR_SCREEN.INTRO;
+        if (AbstractDungeon.ascensionLevel >= 15) {
+            this.damage = MathUtils.round((float)AbstractDungeon.player.maxHealth * A_2_HP_LOSS_PERCENT);
+            this.burn.upgrade();
         } else {
-            this.damage = MathUtils.round((float)AbstractDungeon.player.maxHealth * HP_LOSS_PERCENT);// 49
+            this.damage = MathUtils.round((float)AbstractDungeon.player.maxHealth * HP_LOSS_PERCENT);
         }
 
-        if (AbstractDungeon.player.masterDeck.hasUpgradableCards()) {// 52
-            this.imageEventText.setDialogOption(OPTIONS[0] + this.damage + OPTIONS[1]);// 53
+        if (AbstractDungeon.player.masterDeck.hasUpgradableCards()) {
+            this.imageEventText.setDialogOption(OPTIONS[0] + this.damage + OPTIONS[1]);
         } else {
-            this.imageEventText.setDialogOption(OPTIONS[3], true);// 55
+            this.imageEventText.setDialogOption(OPTIONS[3], true);
         }
 
+        this.imageEventText.setDialogOption(OPTIONS[4] + this.card + OPTIONS[5] + this.burnChance + OPTIONS[6], this.burn);
         this.imageEventText.setDialogOption(OPTIONS[2]);
     }
 
@@ -59,73 +66,88 @@ public class BetterShiningEvent extends AbstractImageEvent {
         if (Settings.AMBIANCE_ON) {
             CardCrawlGame.sound.play("EVENT_SHINING");
         }
-
     }
 
     @Override
     protected void buttonEffect(int buttonPressed) {
-        switch(this.screen) {// 70
+        switch(this.screen) {
             case INTRO:
-                if (buttonPressed == 0) {// 72
-                    this.imageEventText.updateBodyText(AGREE_DIALOG);// 73
-                    this.imageEventText.removeDialogOption(1);// 74
-                    this.imageEventText.updateDialogOption(0, OPTIONS[2]);// 75
-                    this.screen = CUR_SCREEN.COMPLETE;// 76
-                    AbstractDungeon.player.damage(new DamageInfo(AbstractDungeon.player, this.damage));// 77
-                    AbstractDungeon.effectList.add(new FlashAtkImgEffect(AbstractDungeon.player.hb.cX, AbstractDungeon.player.hb.cY, AbstractGameAction.AttackEffect.FIRE));// 78
-                    this.upgradeCards();// 83
-                } else {
-                    this.imageEventText.updateBodyText(DISAGREE_DIALOG);// 85
-                    this.imageEventText.removeDialogOption(1);// 86
-                    this.imageEventText.updateDialogOption(0, OPTIONS[2]);// 87
-                    this.screen = CUR_SCREEN.COMPLETE;// 88
-                    AbstractEvent.logMetricIgnored("Shining Light");// 89
+                if (buttonPressed == 0) {
+                    this.imageEventText.updateBodyText(AGREE_DIALOG);
+                    this.imageEventText.clearAllDialogs();
+                    this.imageEventText.setDialogOption(OPTIONS[2]);
+                    this.screen = CUR_SCREEN.COMPLETE;
+                    AbstractDungeon.player.damage(new DamageInfo(AbstractDungeon.player, this.damage));
+                    AbstractDungeon.effectList.add(new FlashAtkImgEffect(AbstractDungeon.player.hb.cX, AbstractDungeon.player.hb.cY, AbstractGameAction.AttackEffect.FIRE));
+                    this.upgradeCards();
+                } else if (buttonPressed == 1){
+                    int roll = AbstractDungeon.miscRng.random(0,99);
+                    if(roll < burnChance){
+                        AbstractDungeon.effectList.add(new ShowCardAndObtainEffect(card, (float) Settings.WIDTH * 0.4F, (float)Settings.HEIGHT / 2.0F, false));
+                        AbstractDungeon.effectList.add(new ShowCardAndObtainEffect(burn, (float)Settings.WIDTH  * 0.6F, (float)Settings.HEIGHT / 2.0F, false));
+                    }
+                    else{
+                        AbstractDungeon.effectList.add(new ShowCardAndObtainEffect(card, (float) Settings.WIDTH * 0.5F, (float)Settings.HEIGHT / 2.0F, false));
+
+                    }
+                    this.imageEventText.updateBodyText(EMBRACE_DIALOG);
+                    this.imageEventText.clearAllDialogs();
+                    this.imageEventText.setDialogOption(OPTIONS[2]);
+                    this.screen = CUR_SCREEN.COMPLETE;
+                    //AbstractEvent.logMetric
+                } else if (buttonPressed == 2){
+                    this.imageEventText.updateBodyText(DISAGREE_DIALOG);
+                    this.imageEventText.clearAllDialogs();
+                    this.imageEventText.setDialogOption(OPTIONS[2]);
+                    this.screen = CUR_SCREEN.COMPLETE;
+                    //AbstractEvent.logMetricIgnored("Shining Light");
                 }
                 break;
             default:
-                this.openMap();// 93
+                this.openMap();
         }
 
     }
 
     private void upgradeCards() {
-        AbstractDungeon.topLevelEffects.add(new UpgradeShineEffect((float)Settings.WIDTH / 2.0F, (float)Settings.HEIGHT / 2.0F));// 99
-        ArrayList<AbstractCard> upgradableCards = new ArrayList<>();// 100
-        Iterator var2 = AbstractDungeon.player.masterDeck.group.iterator();// 101
+        AbstractDungeon.topLevelEffects.add(new UpgradeShineEffect((float)Settings.WIDTH / 2.0F, (float)Settings.HEIGHT / 2.0F));
+        ArrayList<AbstractCard> upgradableCards = new ArrayList<>();
 
-        while(var2.hasNext()) {
-            AbstractCard c = (AbstractCard)var2.next();
-            if (c.canUpgrade()) {// 102
-                upgradableCards.add(c);// 103
+        for (AbstractCard c : AbstractDungeon.player.masterDeck.group) {
+            if (c.canUpgrade()) {
+                upgradableCards.add(c);
             }
         }
 
-        List<String> cardMetrics = new ArrayList<>();// 107
-        Collections.shuffle(upgradableCards, new Random(AbstractDungeon.miscRng.randomLong()));// 109
-        if (!upgradableCards.isEmpty()) {// 111
-            if (upgradableCards.size() == 1) {// 113
-                (upgradableCards.get(0)).upgrade();// 114
-                cardMetrics.add((upgradableCards.get(0)).cardID);// 115
-                AbstractDungeon.player.bottledCardUpgradeCheck(upgradableCards.get(0));// 116
-                AbstractDungeon.effectList.add(new ShowCardBrieflyEffect((upgradableCards.get(0)).makeStatEquivalentCopy()));// 117
+        List<String> cardMetrics = new ArrayList<>();
+        Collections.shuffle(upgradableCards, new Random(AbstractDungeon.miscRng.randomLong()));
+        if (!upgradableCards.isEmpty()) {
+            if (upgradableCards.size() == 1) {
+                (upgradableCards.get(0)).upgrade();
+                cardMetrics.add((upgradableCards.get(0)).cardID);
+                AbstractDungeon.player.bottledCardUpgradeCheck(upgradableCards.get(0));
+                AbstractDungeon.effectList.add(new ShowCardBrieflyEffect((upgradableCards.get(0)).makeStatEquivalentCopy()));
             } else {
-                (upgradableCards.get(0)).upgrade();// 119
-                (upgradableCards.get(1)).upgrade();// 120
-                cardMetrics.add((upgradableCards.get(0)).cardID);// 121
-                cardMetrics.add((upgradableCards.get(1)).cardID);// 122
-                AbstractDungeon.player.bottledCardUpgradeCheck(upgradableCards.get(0));// 123
-                AbstractDungeon.player.bottledCardUpgradeCheck(upgradableCards.get(1));// 124
-                AbstractDungeon.effectList.add(new ShowCardBrieflyEffect((upgradableCards.get(0)).makeStatEquivalentCopy(), (float)Settings.WIDTH / 2.0F - 190.0F * Settings.scale, (float)Settings.HEIGHT / 2.0F));
-                AbstractDungeon.effectList.add(new ShowCardBrieflyEffect((upgradableCards.get(1)).makeStatEquivalentCopy(), (float)Settings.WIDTH / 2.0F + 190.0F * Settings.scale, (float)Settings.HEIGHT / 2.0F));
+                (upgradableCards.get(0)).upgrade();
+                (upgradableCards.get(1)).upgrade();
+                cardMetrics.add((upgradableCards.get(0)).cardID);
+                cardMetrics.add((upgradableCards.get(1)).cardID);
+                AbstractDungeon.player.bottledCardUpgradeCheck(upgradableCards.get(0));
+                AbstractDungeon.player.bottledCardUpgradeCheck(upgradableCards.get(1));
+                AbstractDungeon.effectList.add(new ShowCardBrieflyEffect((upgradableCards.get(0)).makeStatEquivalentCopy(),
+                        (float)Settings.WIDTH / 2.0F - 190.0F * Settings.scale, (float)Settings.HEIGHT / 2.0F));
+                AbstractDungeon.effectList.add(new ShowCardBrieflyEffect((upgradableCards.get(1)).makeStatEquivalentCopy(),
+                        (float)Settings.WIDTH / 2.0F + 190.0F * Settings.scale, (float)Settings.HEIGHT / 2.0F));
             }
         }
 
-        AbstractEvent.logMetric("Shining Light", "Entered Light", null, null, null, cardMetrics, null, null, null, this.damage, 0, 0, 0, 0, 0);// 137
+        //AbstractEvent.logMetric("Shining Light", "Entered Light", null, null, null, cardMetrics, null, null, null, this.damage, 0, 0, 0, 0, 0);
     }
 
     static {
         AGREE_DIALOG = DESCRIPTIONS[1];
         DISAGREE_DIALOG = DESCRIPTIONS[2];
+        EMBRACE_DIALOG = DESCRIPTIONS[3];
     }
 
     private enum CUR_SCREEN {
