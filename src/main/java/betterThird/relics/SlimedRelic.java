361481fa9a9c02bce2adaa8bc71bff17cad99b7d
nferris165 <51347535+nferris165@users.noreply.github.com>
2020-08-18 15:02:57 -0400

    Slimed Relic
    added functionality
    added absorb keyword
    added text

--- src/main/java/betterThird/relics/SlimedRelic.java
+++ src/main/java/betterThird/relics/SlimedRelic.java
@@ -4,17 +4,22 @@ import basemod.abstracts.CustomRelic;
 import betterThird.BetterThird;
 import betterThird.util.TextureLoader;
 import com.badlogic.gdx.graphics.Texture;
+import com.evacipated.cardcrawl.mod.stslib.relics.OnLoseBlockRelic;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.MakeTempCardInDiscardAction;
 import com.megacrit.cardcrawl.actions.common.MakeTempCardInDrawPileAction;
+import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.cards.status.Slimed;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.PowerTip;
+import com.megacrit.cardcrawl.powers.StrengthPower;
 
 import static betterThird.BetterThird.makeRelicOutlinePath;
 import static betterThird.BetterThird.makeRelicPath;
 
-public class SlimedRelic extends CustomRelic {
+public class SlimedRelic extends CustomRelic implements OnLoseBlockRelic {
 
     public static final String ID = BetterThird.makeID("SlimedRelic");
 
@@ -25,13 +30,68 @@ public class SlimedRelic extends CustomRelic {
     public SlimedRelic() {
         super(ID, IMG, OUTLINE, RelicTier.SPECIAL, LandingSound.MAGICAL);
 
-        this.counter = -1;
+        this.counter = 0;
     }
 
     @Override
     public void atBattleStart() {
+        this.counter = 0;
         AbstractDungeon.actionManager.addToBottom(new MakeTempCardInDiscardAction(new Slimed(), 1));
         AbstractDungeon.actionManager.addToBottom(new MakeTempCardInDrawPileAction(new Slimed(), 1, true, false, false));
+    }
+
+    @Override
+    public int onAttacked(DamageInfo info, int damageAmount) {
+        if (info.owner != null && info.type != DamageInfo.DamageType.HP_LOSS && info.type != DamageInfo.DamageType.THORNS && damageAmount > 1) {
+            if(this.counter == 4){
+                this.stopPulse();
+                this.flash();
+                this.addToBot(new RelicAboveCreatureAction(AbstractDungeon.player, this));
+                AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(info.owner, info.owner, new StrengthPower(info.owner, -1)));
+                this.counter = 0;
+                return 0;
+            }
+            else{
+                this.counter++;
+                if(this.counter == 4){
+                    this.beginLongPulse();
+                }
+                return damageAmount;
+            }
+
+        } else {
+            return damageAmount;
+        }
+    }
+
+    @Override
+    public int onLoseBlock(DamageInfo info, int damageAmount) {
+        if (info.owner != null && info.type != DamageInfo.DamageType.HP_LOSS && info.type != DamageInfo.DamageType.THORNS && damageAmount <= AbstractDungeon.player.currentBlock) {
+            if(this.counter == 4){
+                this.stopPulse();
+                this.flash();
+                this.addToBot(new RelicAboveCreatureAction(AbstractDungeon.player, this));
+                AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(info.owner, info.owner, new StrengthPower(info.owner, -1)));
+                this.counter = 0;
+                return 0;
+            }
+            else{
+                this.counter++;
+                if(this.counter == 4){
+                    this.beginLongPulse();
+                }
+                return damageAmount;
+            }
+
+        } else {
+            return damageAmount;
+        }
+    }
+
+    @Override
+    public void onVictory() {
+        this.counter = 0;
+        this.stopPulse();
     }
 
     @Override
